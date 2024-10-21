f193305658a5b7c5106e97a50fae0553c075b4a6
AllanVitor <165438505+AllanVitorM@users.noreply.github.com>
2024-10-21 19:36:25 -0300

    Update AlterTable

--- EscoladeMusica_AlterTable.sql
+++ EscoladeMusica_AlterTable.sql
@@ -17,4 +17,13 @@ alter table orquestra
 	add column Final_Apres datetime(6) null after Data_Apres;
     
 alter table musicos
-	add column Sexo char(1) null after Instrumento_Fav
+	add column Sexo char(1) null after Instrumento_Fav;
+    
+alter table Instrumentos
+	add column Marca varchar(25) null after Categoria;
+    
+alter table Sinfonia 
+	add column duracao time null after dt_criacao;
+    
+alter table Musicos
+	add column experiencia varchar(25) null after sexo;
